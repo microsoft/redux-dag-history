2fb1e33ea1116abaee2846bc3226078564df094c
darthtrevino <darthtrevino@gmail.com>
2016-05-31 15:40:07 -0700

    Update TSConfig

--- lib/ActionCreators.d.ts
+++ lib/ActionCreators.d.ts
@@ -1,7 +1,7 @@
-export declare const clear: any;
-export declare const undo: any;
-export declare const redo: any;
-export declare const jumpToState: any;
-export declare const jumpToBranch: any;
-export declare const createBranch: any;
-export declare const squash: any;
+export declare const clear: (...args: void[]) => ReduxActions.Action<void>;
+export declare const undo: (...args: void[]) => ReduxActions.Action<void>;
+export declare const redo: (...args: void[]) => ReduxActions.Action<void>;
+export declare const jumpToState: (...args: string[]) => ReduxActions.Action<string>;
+export declare const jumpToBranch: (...args: string[]) => ReduxActions.Action<string>;
+export declare const createBranch: (...args: string[]) => ReduxActions.Action<string>;
+export declare const squash: (...args: void[]) => ReduxActions.Action<void>;
