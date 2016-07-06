531521c9428d256a681abf40f40438a20d77dab5
darthtrevino <darthtrevino@gmail.com>
2016-07-06 14:01:24 -0700

    Change ActionCreators.createBranch Payload Type to 'string'
    The createBranch action accepts the branch name. The branch ID is generated by the system.

--- src/ActionCreators.ts
+++ src/ActionCreators.ts
@@ -7,5 +7,5 @@ export const undo = createAction<void>(ActionTypes.UNDO);
 export const redo = createAction<void>(ActionTypes.REDO);
 export const jumpToState = createAction<StateId>(ActionTypes.JUMP_TO_STATE);
 export const jumpToBranch = createAction<BranchId>(ActionTypes.JUMP_TO_BRANCH);
-export const createBranch = createAction<BranchId>(ActionTypes.CREATE_BRANCH);
+export const createBranch = createAction<string>(ActionTypes.CREATE_BRANCH);
 export const squash = createAction<void>(ActionTypes.SQUASH);
