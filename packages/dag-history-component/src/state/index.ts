2ead48ff0945ab38bac504103c48ff3ce0fcf81a
Chris Trevino <darthtrevino@users.noreply.github.com>
2017-03-01 13:26:14 -0800

    Include configuration.ts in state/index (#21)
    * Include configuration.ts in state/index
    
    * Update import casing

--- packages/dag-history-component/src/state/index.ts
+++ packages/dag-history-component/src/state/index.ts
@@ -1,6 +1,7 @@
 import * as Actions from './actions/creators';
 import * as ActionTypes from './actions/types';
 import reducer from './reducers';
+import Configuration from './Configuration';
 
 export {
   Actions,
