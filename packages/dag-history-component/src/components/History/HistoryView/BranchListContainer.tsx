ee1b1c5cfacbb397041d1e61c90ac6679a717f1f
Chris Trevino <darthtrevino@gmail.com>
2017-03-08 15:01:49 -0800

    Reverse the branch list

--- packages/dag-history-component/src/components/History/HistoryView/BranchListContainer.tsx
+++ packages/dag-history-component/src/components/History/HistoryView/BranchListContainer.tsx
@@ -131,7 +131,8 @@ export default class BranchListContainer extends React.Component<IBranchListCont
       !pinnedState ||
       branch.active ||
       isNumber(branch.successorDepth)
-    ));
+    ))
+    .reverse();
   }
 
   render() {
